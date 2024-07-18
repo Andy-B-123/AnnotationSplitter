ffc7fdd8b926e973aef98481d725cc62a62cc311
Andy-B-123 <35752341+Andy-B-123@users.noreply.github.com>
2024-07-18 15:21:31 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="AnnotationSplitter",
-    version="0.0.9",
+    version="0.1.0",
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     include_package_data=True,
-    install_requires=[
-        "icecream>=2.1",
-        "pandas",
-        "tqdm",
-        "requests",
-        "cogent3>=2024.2.5a1",
-        "pyfaidx>=0.8",
-        "biopython>=1.83",
-        "matplotlib"  # Add this if you are using matplotlib
-    ],
     entry_points={
         'console_scripts': [
             'AnnotationSplitter=src.main:main',
@@ -23,7 +13,7 @@ setup(
     },
     author="Andreas Bachler",
     author_email="Andy.Bachler@example.com",
-    description="A simple bioinformatics script.",
+    description="A simple Python project example.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/Andy-B-123/AnnotationSplitter",
@@ -32,5 +22,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.11',
+    python_requires='>=3.6',
 )
