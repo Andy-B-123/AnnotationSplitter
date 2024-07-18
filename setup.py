e0d09405f8170b95e75aa564d5b686e4944e3c91
Andy-B-123 <35752341+Andy-B-123@users.noreply.github.com>
2024-07-18 15:36:55 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AnnotationSplitter",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     include_package_data=True,
@@ -18,7 +18,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'AnnotationSplitter=src.main:main',
+            'AnnotationSplitter=main:main',
         ],
     },
     author="Andreas Bachler",
